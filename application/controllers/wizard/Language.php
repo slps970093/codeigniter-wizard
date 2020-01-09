8c8f20b892c687f54e4c1fdd6b2a071207fd5103
slps970093 <slps970093@gmail.com>
2020-01-09 22:02:45 +0800

    first commit

---
+++ application/controllers/wizard/Language.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * 語系
+ *
+ * Class Language
+ *
+ * @another 小周
+ */
+
+class Language extends CI_Controller
+{
+
+}
---
+++ application/controllers/wizard/Projects.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * 專案設定 Controller
+ *
+ * Class Projects
+ *
+ * @another 小周
+ */
+
+class Projects extends CI_Controller
+{
+
+    public function index()
+    {
+
+    }
+
+}
